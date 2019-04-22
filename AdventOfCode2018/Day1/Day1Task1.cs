8aaf7356c81f2c1ca5ff8517da2cf5fbdbe20c04
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-04-22 10:17:07 +0200

    Day1

---
+++ AdventOfCode2018/Day1/Day1Task1.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode2018.Day1
+{
+    using System.Linq;
+    using Day;
+    using Readers;
+
+    public class Day1Task1 : BaseDay
+    {
+        public Day1Task1(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            var input = GetInput();
+            var numbers = input.Select(s => int.Parse(s));
+            var result = numbers.Sum();
+            return result.ToString();
+        }
+    }
+}
---
+++ AdventOfCode2018/Day1/Day1Task2.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2018.Day1
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Day;
+    using Readers;
+
+    public class Day1Task2 : BaseDay
+    {
+        public Day1Task2(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            var input = GetInput();
+            var numbers = input.Select(s => int.Parse(s)).ToArray();
+
+            var reachedFrequencies = new HashSet<int>();
+
+            int index = 0;
+            var frequency = 0;
+            reachedFrequencies.Add(frequency);
+            while (true)
+            {
+                frequency += numbers[index];
+
+                if (reachedFrequencies.Contains(frequency))
+                {
+                    return frequency.ToString();
+                }
+
+                reachedFrequencies.Add(frequency);
+
+                if (index == numbers.Length - 1)
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+        }
+    }
+}
