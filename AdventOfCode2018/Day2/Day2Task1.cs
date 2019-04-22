f8c02848131a1b25cce36fa055b7138359b87684
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-04-22 10:17:13 +0200

    Day2

---
+++ AdventOfCode2018/Day2/Day2Task1.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode2018.Day2
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http.Headers;
+    using Day;
+    using Readers;
+
+    public class Day2Task1 : BaseDay
+    {
+        public Day2Task1(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            var input = GetInput();
+            var data = input;
+
+            var twos = 0;
+            var threes = 0;
+
+            foreach (var s in data)
+            {
+                var letters = s.ToCharArray()
+                    .GroupBy(x => x)
+                    .Select(x => new { Letter = x.Key, Count = x.Count() })
+                    .ToArray();
+
+                if (letters.Any(x => x.Count == 2))
+                {
+                    twos++;
+                }
+
+                if (letters.Any(x => x.Count == 3))
+                {
+                    threes++;
+                }
+            }
+
+            var result = twos * threes;
+            return result.ToString();
+        }
+    }
+}
---
+++ AdventOfCode2018/Day2/Day2Task2.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode2018.Day2
+{
+    using System.Linq;
+    using System.Text;
+    using Day;
+    using Readers;
+
+    public class Day2Task2 : BaseDay
+    {
+        public Day2Task2(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            var input = GetInput();
+            var data = input
+                .ToArray();
+
+            var length = data.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    var (differByOneCharacter, differentCharacterIndex) = DifferByOnlyOneCharacter(data[i], data[j]);
+                    if (differByOneCharacter)
+                    {
+                        var result = data[i].Remove(differentCharacterIndex.Value, 1);
+                        return result;
+                    }
+                }
+            }
+
+            return "Something went wrong";
+        }
+
+        public (bool, int?) DifferByOnlyOneCharacter(string first, string second)
+        {
+            int? differentCharacterIndex = null;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] == second[i]) continue;
+
+                if (differentCharacterIndex.HasValue)
+                {
+                    return (false, null);
+                }
+                else
+                {
+                    differentCharacterIndex = i;
+                }
+            }
+
+            return (true, differentCharacterIndex);
+        }
+    }
+}
