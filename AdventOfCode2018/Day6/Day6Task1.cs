99865a02ba9674fa03ce1be2491d2249894bd84f
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-04-26 16:09:17 +0200

    Day6

---
+++ AdventOfCode2018/Day6/Day6Task1.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2018.Day6
+{
+    using Day;
+    using Readers;
+
+    public class Day6Task1 : BaseDay
+    {
+        public Day6Task1(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2018/Day6/Day6Task2.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2018.Day6
+{
+    using Day;
+    using Readers;
+
+    public class Day6Task2 : BaseDay
+    {
+        public Day6Task2(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
