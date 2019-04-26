6ffc4cde19e94fb90f476fa44be95600836a9046
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-04-26 16:09:29 +0200

    Input reader and inputs

---
+++ AdventOfCode2018/Readers/IInputReader.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2018.Readers
+{
+    public interface IInputReader
+    {
+        string GetInput(int day);
+        string GetInput(int day, int task);
+    }
+}
---
+++ AdventOfCode2018/Readers/InputReader.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2018.Readers
+{
+    using System.IO;
+
+    public class InputReader : IInputReader
+    {
+        private const string _sameInputsPath = "..//..//..//..//Inputs//{0}.txt";
+
+        private const string _otherInputsPath = "..//..//..//..//Inputs//{0}_{1}.txt";
+
+        public string GetInput(int day)
+        {
+            var filePath = string.Format(_sameInputsPath, day);
+            var input = GetInputFromFile(filePath);
+            return input;
+        }
+
+        public string GetInput(int day, int task)
+        {
+            var filePath = string.Format(_otherInputsPath, day, task);
+            var input = GetInputFromFile(filePath);
+            return input;
+        }
+
+        private string GetInputFromFile(string filePath)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    var data = streamReader.ReadToEnd();
+                    return data;
+                }
+            }
+        }
+    }
+}
