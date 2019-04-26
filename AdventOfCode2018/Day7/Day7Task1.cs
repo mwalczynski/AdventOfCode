cd98758aa84503757a96b7b9b49dcaacf729a5e5
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-04-27 00:08:58 +0200

    changes v2

---
+++ AdventOfCode2018/Day7/Day7Task1.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2018.Day7
+{
+    using Day;
+    using Readers;
+
+    public class Day7Task1 : BaseDay
+    {
+        public Day7Task1(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2018/Regexes.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2018
+{
+    using System.Text.RegularExpressions;
+
+    public static class Regexes
+    {
+        public static readonly Regex TaskRegex = new Regex(@"Day\d+Task[12]");
+        public static readonly Regex NumbersRegex = new Regex(@"\d+");
+    }
+}
