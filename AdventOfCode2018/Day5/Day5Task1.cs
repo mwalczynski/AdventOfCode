0977deaa30bae8d14ebd628793c099e0e4fe2147
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-04-26 16:09:08 +0200

    Day5

---
+++ AdventOfCode2018/Day5/Day5Task1.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode2018.Day5
+{
+    using System;
+    using System.Linq;
+    using Day;
+    using Readers;
+
+    public class Day5Task1 : BaseDay
+    {
+        public Day5Task1(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            var input = GetInput();
+            var data = input.Single();
+
+            var index = 0;
+            var currentDataLength = data.Length;
+            while (index < currentDataLength - 1)
+            {
+                var first = data[index];
+                var second = data[index + 1];
+
+                if (AreTheSameWithOppositePolarities(first, second))
+                {
+                    data = data.Remove(index, 2);
+                    currentDataLength = currentDataLength - 2;
+                    index = Math.Max(0, index - 1);
+                    continue;
+                }
+
+                index++;
+            }
+
+            var result = data.Length;
+            return result.ToString();
+        }
+
+        private bool AreTheSameWithOppositePolarities(char first, char second)
+        {
+            int firstValue = first;
+            int secondValue = second;
+
+            var result = Math.Abs(firstValue - secondValue) == 32;
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2018/Day5/Day5Task2.cs
@@ -0,0 +1,76 @@
+﻿namespace AdventOfCode2018.Day5
+{
+    using System;
+    using System.Linq;
+    using Day;
+    using Readers;
+
+    public class Day5Task2 : BaseDay
+    {
+        public Day5Task2(IInputReader reader) : base(reader)
+        {
+        }
+
+        public override string GetResult()
+        {
+            var input = GetInput();
+            var data = input.Single();
+
+            var chars = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (char)i).ToArray();
+
+            var charToRemove = chars
+                .Aggregate((l, c) =>
+                    ProducePolymer(data, l).Length >
+                    ProducePolymer(data, c).Length
+                ? c
+                : l);
+
+            var fullyReactedPolymer = ProducePolymer(data, charToRemove);
+            var result = fullyReactedPolymer.Length;
+            return result.ToString();
+        }
+
+        private string ProducePolymer(string data, char charToRemove)
+        {
+            var lowerCharToCompare = char.ToLower(charToRemove);
+            var upperCharToCompare = char.ToUpper(charToRemove);
+            var chars = data.Where(c => !(c == lowerCharToCompare || c == upperCharToCompare)).ToArray();
+            var polymer = new string(chars);
+            var fullyReactedPolymer = FullyReactPolymer(polymer);
+            return fullyReactedPolymer;
+        }
+
+        private string FullyReactPolymer(string polymer)
+        {
+            var index = 0;
+            var currentDataLength = polymer.Length;
+            while (index < currentDataLength - 1)
+            {
+                var first = polymer[index];
+                var second = polymer[index + 1];
+
+                if (AreTheSameWithOppositePolarities(first, second))
+                {
+                    polymer = polymer.Remove(index, 2);
+                    currentDataLength = currentDataLength - 2;
+                    index = Math.Max(0, index - 1);
+                    continue;
+                }
+
+                index++;
+            }
+
+            var result = polymer;
+            return result;
+        }
+
+        private bool AreTheSameWithOppositePolarities(char first, char second)
+        {
+            int firstValue = first;
+            int secondValue = second;
+
+            var result = Math.Abs(firstValue - secondValue) == 32;
+            return result;
+        }
+    }
+}
