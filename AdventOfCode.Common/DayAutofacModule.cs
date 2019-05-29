14d9f3410e568b79a703169a4b846be4ea6e9209
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-05-29 22:06:02 +0200

    Base autofac module

---
+++ AdventOfCode.Common/DayAutofacModule.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode.Common
+{
+    using System.Reflection;
+    using Autofac;
+    using Module = Autofac.Module;
+
+    public class DayAutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetAssembly(this.GetType()))
+                .Where(t => Regexes.TaskRegex.IsMatch(t.Name))
+                .Named<BaseDay>(t => string.Join(";", ClassIndexProvider.GetStringNumbers(t.Name)))
+                .Keyed<BaseDay>(t => ClassIndexProvider.GetStringNumbers(t.Assembly.GetName().Name)[0]);
+        }
+    }
+}
