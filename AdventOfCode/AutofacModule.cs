a97b510465560e626ca30509de707c16ab8cbabb
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-05-29 23:10:07 +0200

    Attempt to load assemblies automaticaly

---
+++ AdventOfCode/AutofacModule.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode
+{
+    using Autofac;
+    using Autofac.Features.AttributeFilters;
+    using Common.Readers;
+
+    class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => Config.Year).Keyed<int>(Config.YearConfigKey);
+            builder.RegisterType<DaysManager>().As<IDaysManager>().WithAttributeFiltering();
+            builder.RegisterType<InputReader>().As<IInputReader>();
+        }
+    }
+}
--- AdventOfCode/ContainerBuilderFactory.cs
+++ AdventOfCode/ContainerBuilderFactory.cs
@@ -1,8 +1,12 @@
 ﻿namespace AdventOfCode
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text.RegularExpressions;
     using Autofac;
-    using Autofac.Features.AttributeFilters;
-    using Common.Readers;
+    using Module = Autofac.Module;
 
     public class ContainerBuilderFactory
     {
@@ -10,15 +14,41 @@
         {
             var containerBuilder = new ContainerBuilder();
 
-            containerBuilder.RegisterModule(new AdventOfCode2016.AutofacModule());
-            containerBuilder.RegisterModule(new AdventOfCode2018.AutofacModule());
-
-            containerBuilder.Register(c => Config.Year).Keyed<int>(Config.YearConfigKey);
-            containerBuilder.RegisterType<DaysManager>().As<IDaysManager>().WithAttributeFiltering();
-            containerBuilder.RegisterType<InputReader>().As<IInputReader>();
+            var assemblies = GetAssemblies().ToArray();
+            containerBuilder.RegisterAssemblyModules<Module>(assemblies);
 
             var container = containerBuilder.Build();
             return container;
+        }
+
+        public static IEnumerable<Assembly> GetAssemblies()
+        {
+            var hashSet = new HashSet<string>();
+            var stack = new Stack<Assembly>();
+
+            var entryAssembly = Assembly.GetEntryAssembly();
+            var entryAssemblyName = entryAssembly.GetName().Name;
+            stack.Push(entryAssembly);
+
+            var regex = new Regex($"{entryAssemblyName}\\d+");
+
+            do
+            {
+                var assembly = stack.Pop();
+
+                yield return assembly;
+
+                foreach (var referencedAssembly in asse())
+                {
+                    if (!regex.IsMatch(referencedAssembly.Name)) continue;
+                    if (hashSet.Contains(referencedAssembly.FullName)) continue;
+
+                    stack.Push(Assembly.Load(referencedAssembly));
+                    hashSet.Add(referencedAssembly.FullName);
+                }
+
+            }
+            while (stack.Count > 0);
         }
     }
 }
